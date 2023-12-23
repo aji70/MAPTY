df423003c1a58cf7f8d210aa59c8a5985764ab50
aji70 <113303791+aji70@users.noreply.github.com>
2023-12-23 12:16:35 +0100

    MAPTY

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+
+if (navigator.geolocation)
+navigator.geolocation.getCurrentPosition(function(position){
+    // const {longitude,latitude} = position.coords;
+   const latitude = 10.473222;
+   const longitude = 7.447012
+
+    console.log(latitude, longitude);
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude]
+    // const coords = [9.060352, 7.471104]
+    // const coords = [7.471104, 9.060352]
+
+    const map = L.map('map').setView(coords, 12);
+
+L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker(coords).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+
+    map.on()
+
+}, function(){
+    alert('cannot get your Location');
+})
