582af9b45c7a97a0cbba8e2ec36565de09f1e9e3
aji70 <113303791+aji70@users.noreply.github.com>
2023-12-28 22:16:04 +0100

    Final Update

--- script.js
+++ script.js
@@ -31,7 +31,8 @@ L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker(coords).addTo(map)
+L.marker(coords)
+    .addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
     .openPopup();
 
